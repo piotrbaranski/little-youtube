d05f8c715f4de44cd69f84c6721044eca5bb5a02
piotrbaranski <p.baranski@yahoo.pl>
2018-02-09 15:02:34 +0100

    Start working on VideoListItem component. Use map function on all video items get from YouTube and show them in the list. Work in progress.

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import VideoListItem from './VideoListItem'
 
-const VideoList = () => {
+const VideoList = (props) => {
+
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video={video} />
+  })
+
   return (
     <ul>
-
+      {videoItems}
     </ul>
   )
 }
