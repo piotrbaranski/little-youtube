6b91042afc3563cfd9958e23f17cf12df03cdbc3
piotrbaranski <p.baranski@yahoo.pl>
2018-02-09 14:47:20 +0100

    Start working on VideoList component. Add prop "videos" to this component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/SearchBar'
+import VideoList from './components/VideoList'
 
 const API_KEY = 'AIzaSyDo0LvUfwYc1cQu08fklNVOTnLadLp8UXo';
 
@@ -22,6 +23,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar/>
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
---
+++ src/components/VideoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoList = () => {
+  return (
+    <ul>
+
+    </ul>
+  )
+}
+
+export default VideoList
