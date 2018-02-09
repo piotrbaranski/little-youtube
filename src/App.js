374a1dab045118fbbed0f6d1dfd6d491bcc15111
piotrbaranski <p.baranski@yahoo.pl>
2018-02-09 14:39:19 +0100

    Add search bar its own state. Make input to handle state changes. Add App its own state. Get data from YouTube and log them in the console.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/SearchBar'
 
 const API_KEY = 'AIzaSyDo0LvUfwYc1cQu08fklNVOTnLadLp8UXo';
 
 class App extends Component {
+
+  state = {
+    videos: []
+  }
+
+  componentDidMount() {
+    YTSearch({key: API_KEY, term: 'cat'}, (videos) => {
+      this.setState({videos})
+      console.log(videos)
+    });
+  }
+
   render() {
     return (
       <div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,8 +2,21 @@ import React, {Component} from 'react'
 
 
 class SearchBar extends Component {
+
+  state = {
+    term: ''
+  }
+
+  handleInputChange = event => this.setState({term: event.target.value});
+
   render() {
-    return <input/>;
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={this.handleInputChange}/>
+      </div>
+    )
   }
 }
 
